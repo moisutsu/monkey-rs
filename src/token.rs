3ee55d3e712df284a651d314927267af1610e958
moisutsu <moisutsu@gmail.com>
2020-09-24 21:39:04 +0900

    Add tokens

---
+++ src/token.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, Clone)]
+enum Token {
+    Illegal,       // unknown token
+    Eof,           // end of file
+    Ident(String), // identifier
+    Int(i32),      // integer literal
+    Assign,        // =
+    Plus,          // +
+    Comma,         // ,
+    Semicolon,     // ;
+    Lparen,        // (
+    Rparen,        // )
+    Lbrace,        // {
+    Rbrace,        // }
+    Function,      // fn
+    Let,           // let
+}
