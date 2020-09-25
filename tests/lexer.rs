b3a058007b94cd01a7676f0e21dbead65e5f1cd8
moisutsu <moisutsu@gmail.com>
2020-09-26 00:19:40 +0900

    Extend test for next_token

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -2,9 +2,52 @@ use monkey::{Lexer, Token::*};
 
 #[test]
 fn test_next_token() {
-    let input = "=+(){},;".to_string();
+    let input = "let five = 5;
+    let ten = 10;
+
+    let add = fn(x, y) {
+        x + y;
+    };
+    let result = add(five, ten);"
+        .to_string();
     let answers = vec![
-        Assign, Plus, Lparen, Rparen, Lbrace, Rbrace, Comma, Semicolon,
+        Let,
+        Ident("five".to_string()),
+        Assign,
+        Int(5),
+        Semicolon,
+        Let,
+        Ident("ten".to_string()),
+        Assign,
+        Int(10),
+        Semicolon,
+        Let,
+        Ident("add".to_string()),
+        Assign,
+        Function,
+        Lparen,
+        Ident("x".to_string()),
+        Comma,
+        Ident("y".to_string()),
+        Rparen,
+        Lbrace,
+        Ident("x".to_string()),
+        Plus,
+        Ident("y".to_string()),
+        Semicolon,
+        Rbrace,
+        Semicolon,
+        Let,
+        Ident("result".to_string()),
+        Assign,
+        Ident("add".to_string()),
+        Lparen,
+        Ident("five".to_string()),
+        Comma,
+        Ident("ten".to_string()),
+        Rparen,
+        Semicolon,
+        Eof,
     ];
     let mut lexer = Lexer::new(input);
     for i in 0..answers.len() {
