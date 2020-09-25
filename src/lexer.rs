69766bfa9eb455a22532a2f175e9b72b98c07794
moisutsu <moisutsu@gmail.com>
2020-09-25 23:48:22 +0900

    Add read_char to Lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,7 +3,7 @@ pub struct Lexer {
     input: String,
     position: usize,
     read_position: usize,
-    ch: char,
+    ch: Option<char>,
 }
 
 impl Lexer {
@@ -12,5 +12,14 @@ impl Lexer {
             input,
             ..Lexer::default()
         }
+    }
+    pub fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = None;
+        } else {
+            self.ch = self.input.chars().nth(self.read_position); // O(n)
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
     }
 }
