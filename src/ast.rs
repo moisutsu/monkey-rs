6dd8e0a3c9807ba75cfd04553a7a432a84409bfc
moisutsu <moisutsu@gmail.com>
2021-01-21 21:21:58 +0900

    Use enum for ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,79 +1,69 @@
-use crate::Token;
-
-trait Node {
-    fn token_literal(&self) -> String;
+#[derive(Debug, Clone)]
+pub enum Node {
+    Program(Program),
+    Statement(Statement),
+    Expression(Expression),
 }
 
-trait Statement {
-    fn statement_node(&self);
-}
-
-trait Expression {
-    fn expression_node(&self);
-}
-
-struct Program<S>
-where
-    S: Statement + Node,
-{
-    statements: Vec<S>,
-}
-
-impl<S> Node for Program<S>
-where
-    S: Node + Statement,
-{
-    fn token_literal(&self) -> String {
-        if self.statements.len() > 0 {
-            self.statements[0].token_literal()
-        } else {
-            "".to_string()
+impl Node {
+    pub fn token_literal(&self) -> String {
+        match self {
+            Self::Program(program) => {
+                if program.statements.len() > 0 {
+                    program.statements[0].token_literal()
+                } else {
+                    "".to_string()
+                }
+            }
+            Self::Statement(statement) => statement.token_literal(),
+            Self::Expression(expression) => expression.token_literal(),
         }
     }
 }
 
-struct LetStatement<E>
-where
-    E: Node + Expression,
-{
-    token: Token, // Token::Let
-    name: Identifier,
-    value: E,
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub statements: Vec<Statement>,
 }
 
-impl<E> Node for LetStatement<E>
-where
-    E: Node + Expression,
-{
-    fn token_literal(&self) -> String {
-        match self.token {
-            Token::Let => "let".to_string(),
-            _ => unreachable!(),
+#[derive(Debug, Clone)]
+pub enum Statement {
+    LetStatement(LetStatement),
+}
+
+impl Statement {
+    pub fn token_literal(&self) -> String {
+        match self {
+            Self::LetStatement(_) => "let".to_string(),
         }
     }
 }
 
-impl<E> Statement for LetStatement<E>
-where
-    E: Node + Expression,
-{
-    fn statement_node(&self) {}
+#[derive(Debug, Clone)]
+pub enum Expression {
+    Identifier(Identifier),
 }
 
-struct Identifier {
-    token: Token, // Token::Ident
-    value: String,
-}
-
-impl Node for Identifier {
+impl Expression {
     fn token_literal(&self) -> String {
-        match &self.token {
-            Token::Ident(ident) => ident.clone(),
-            _ => unreachable!(),
+        match self {
+            Self::Identifier(expression) => expression.token_literal(),
         }
     }
 }
 
-impl Expression for Identifier {
-    fn expression_node(&self) {}
+#[derive(Debug, Clone)]
+pub struct LetStatement {
+    pub name: Identifier,
+    value: Expression,
+}
+
+#[derive(Debug, Clone)]
+pub struct Identifier(pub String);
+
+impl Identifier {
+    fn token_literal(&self) -> String {
+        let Identifier(ident) = self;
+        ident.clone()
+    }
 }
